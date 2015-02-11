35e7a86c2e76c8ba7e543dd6fbe23450d4e2a7ea
Karim El Aktaa <karim.elaktaa@gmail.com>
2015-02-11 22:50:32 +0100

    add a cube in the scene

--- basic_00/script.js
+++ basic_00/script.js
@@ -6,16 +6,27 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color:0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var material = new THREE.MeshBasicMaterial({color:0x00fff0});
+var cube1 = new THREE.Mesh(geometry, material);
+var cube2 = new THREE.Mesh(geometry, material);
+var cube3 = new THREE.Mesh(geometry, material);
+scene.add(cube1);
+scene.add(cube2);
+scene.add(cube3);
 
+cube2.position.x = 5;
+cube3.position.y = 5;
 camera.position.z = 10;
 
 function render() {
 	requestAnimationFrame(render);
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	cube1.rotation.x += 0.01;
+	cube1.rotation.y += 0.01;
+	cube2.rotation.x += 0.1;
+	cube2.rotation.y += 0.01;
+	cube3.rotation.x += 0.01;
+	cube3.rotation.y += 0.1;
+
 	renderer.render(scene, camera);
 }
 
