18caee2101bf30a5868c59ade7dad0ef1d9aa335
Karim El Aktaa <karim.elaktaa@gmail.com>
2015-02-16 00:34:52 +0100

    cleaning the main script for basic_01

--- basic_01/main.js
+++ basic_01/main.js
@@ -1,28 +1,32 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var scene, camera, renderer;
 var allCubes = [];
 
-function createSceneElements(){
+function init(){
+	// init the basic elements
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+	camera.position.z = 10;
+
+	// light init
 	var light = new THREE.DirectionalLight(0xffffff, 1);
 	light.position.set(1, 1, 1).normalize();
 	scene.add(light);
-	var geometryCube = new THREE.BoxGeometry(1, 1, 1);
-	var material = new THREE.MeshBasicMaterial({color:0x00fff1});
-	var cube = new THREE.Mesh(geometryCube, material);
-	scene.add(cube);
 
-	camera.position.z = 10;
+	var geometryCube = new THREE.BoxGeometry(1, 1, 1);
+	//var material = new THREE.MeshBasicMaterial({color:0x00fff1});
+
+	// use a range for the random position of the cubes
 	var range = 50;
 	for(i = 0; i < 200; i++){
-		//var grayness = Math.random() * 0.5 + 0.25;	
-		//material.color.setRGB(grayness, grayness, grayness);
-		//cube.grayness = grayness;
-		cube = new THREE.Mesh(geometryCube, new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff }));
+		var cube = new THREE.Mesh(geometryCube, new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff }));
 	    cube.position.set( range * (0.5 - Math.random()), range * (0.5 - Math.random()), range * (0.5 - Math.random()) );
 	    cube.rotation.set( Math.random(), Math.random(), Math.random() );
+		cube.scale.x = Math.random() + 0.7;
+		cube.scale.y = Math.random() + 0.7;
+		cube.scale.z = Math.random() + 0.7;
 		scene.add(cube);
 		allCubes[allCubes.length] = cube;
 	}
@@ -37,15 +41,11 @@ function modifyRotationOfOneCube(element, index, array){
 	element.rotation.set( element.rotation.x + Math.random()/pondRotation, element.rotation.y + Math.random()/pondRotation, element.rotation.y + Math.random()/pondRotation );
 }
 
-function testRayCasting(){
-}
-
 function render(){
 	modifyRotationOfCubes();
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
 }
 
-testRayCasting();
-createSceneElements();
+init();
 render();
